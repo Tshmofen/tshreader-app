17296a1d082ed0bd60e64623b8e33ab264aaf7db
Timofey <timohatimafeikq@gmail.com>
2022-07-16 18:16:47 +0300

    Add base project

---
+++ tshreader/tshreader/tshreader/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using tshreader.ViewModels;
+using Xamarin.Forms;
+
+namespace tshreader.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ tshreader/tshreader/tshreader/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tshreader.Models;
+using tshreader.ViewModels;
+using tshreader.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tshreader.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
